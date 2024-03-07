d13616bf907565204c00efb95199390d33411264
nidhinsankar <nidhinsankar1@gmail.com>
2024-03-07 13:29:32 +0530

    Revert "refactor in readme,index.html"
    This reverts commit 44e9054cd2f4eefb111930505e2d5356e6abd8f3.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ function App() {
   return (
     <>
       <div className="bg-red-400 font-bold text-center">
-        SnapStock- Online MarketPlace
+        <p>
+          A react template repository with tailwindcss, Typescript scaffolded by
+          vite
+        </p>
+        <h2>Feel free to start building your project</h2>
       </div>
     </>
   );

44e9054cd2f4eefb111930505e2d5356e6abd8f3
nidhinsankar <nidhinsankar1@gmail.com>
2024-03-07 13:21:48 +0530

    refactor in readme,index.html

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ function App() {
   return (
     <>
       <div className="bg-red-400 font-bold text-center">
-        <p>
-          A react template repository with tailwindcss, Typescript scaffolded by
-          vite
-        </p>
-        <h2>Feel free to start building your project</h2>
+        SnapStock- Online MarketPlace
       </div>
     </>
   );
